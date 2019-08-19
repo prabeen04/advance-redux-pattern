77528493901bed7d6fdd93a8c3fbaa730a735a16
Prabeen04 <prabeen.strange@gmail.com>
2019-08-19 23:07:20 +0530

    code cleaned

--- src/Container/Carousle.js
+++ src/Container/Carousle.js
@@ -36,34 +36,29 @@ class Example extends Component {
   constructor(props) {
     super(props);
     this.state = { activeIndex: 0 };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
   }
 
-  onExiting() {
+  onExiting = () => {
     this.animating = true;
   }
 
-  onExited() {
+  onExited = () => {
     this.animating = false;
   }
 
-  next() {
+  next = () => {
     if (this.animating) return;
     const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
-  previous() {
+  previous = () => {
     if (this.animating) return;
     const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
 
-  goToIndex(newIndex) {
+  goToIndex = (newIndex) => {
     if (this.animating) return;
     this.setState({ activeIndex: newIndex });
   }
