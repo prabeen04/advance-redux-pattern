8cc6149836e34ec363bd00cf7056de0ecd5f3a42
samliprajapati <samliprajapati9@gmail.com>
2019-08-19 22:34:36 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Carousle from "./Container/Carousle"
 
 function App() {
   return (
     <div>
       <h1>Advance redux pattern</h1>
+      <Carousle/>
     </div>
   );
 }
