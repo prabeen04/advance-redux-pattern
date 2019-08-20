de1b6a6abcab82a40eb9c367f8eac6d6b2b67ae4
Prabeen04 <prabeen.strange@gmail.com>
2019-08-20 19:34:09 +0530

    carousel removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Carousle from "./Container/Carousle"
 
 function App() {
   return (
     <div>
       <h1>Advance redux pattern</h1>
-      <Carousle/>
     </div>
   );
 }
