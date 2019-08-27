cf84dd4aa1da4be11f0aab618d0b8b4c10fc9716
Prabeen04 <prabeen.strange@gmail.com>
2019-08-27 22:18:04 +0530

    useDispatch to toggleTheme

--- src/modules/settings/theme/ThemeSwitch.js
+++ src/modules/settings/theme/ThemeSwitch.js
@@ -1,22 +1,32 @@
-import React, { Fragment } from 'react'
-import { useSelector } from "react-redux";
+import React, { Fragment, useEffect, useCallback } from 'react'
+import { useSelector, useDispatch } from "react-redux";
 import Switch from '@material-ui/core/Switch';
+import { setDarkTheme, setLightTheme } from "./themeAction";
+import { lightTheme, darkTheme } from '../../../config/theme';
 
 export default function ThemeSwitch(props) {
     const themeType = useSelector(({ theme }) => theme.themeType)
+    const dispatch = useDispatch()
+    const toggleTheme = useCallback(handleChange, [dispatch])
+    useEffect(() => {
+        themeType === 'light'
+            ? document.body.style.backgroundColor = '#f4f4f4'
+            : document.body.style.backgroundColor = '#222222'
+    }, [themeType])
 
-    function handleChange(checked) {
-        if (themeType === 'light') {
-
+    function handleChange(e, checked) {
+        console.log(e, checked)
+        if (checked) {
+            setDarkTheme(darkTheme)
         } else {
-
+            setLightTheme(lightTheme)
         }
     }
     return (
         <Fragment>
             <Switch
                 checked={themeType === 'light' ? false : true}
-                onChange={handleChange}
+                onChange={toggleTheme}
                 value="checkedA"
                 inputProps={{ 'aria-label': 'secondary checkbox' }}
             />
