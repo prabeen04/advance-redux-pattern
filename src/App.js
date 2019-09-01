be1c55bf254edb7393f55e6a924ca5e8f9c6ff49
Prabeen04 <prabeen.strange@gmail.com>
2019-09-01 20:42:17 +0530

    ThemeProvider integrated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { ApplicationWrapper } from "./components/UI/Layout";
 import './App.css';
 import ThemeSwitch from './modules/settings/theme/ThemeSwitch';
 
 function App() {
+  const theme = useSelector(({ theme }) => theme.theme)
   return (
-    <div>
-      <h1>Advance redux pattern</h1>
-      <ThemeSwitch/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ApplicationWrapper>
+        <h1>Advance redux pattern</h1>
+        <ThemeSwitch />
+      </ApplicationWrapper>
+    </ThemeProvider>
   );
 }
 
