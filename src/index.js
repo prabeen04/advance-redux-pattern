adc9722f61ddce3a26f881907d1498df2958aedb
Prabeen04 <prabeen.strange@gmail.com>
2019-09-06 22:14:56 +0530

    useRouter hook created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router as BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import './index.css';
@@ -12,13 +13,11 @@ import { SENTRY_URL } from './config/config';
 sentry.initialize(SENTRY_URL)
 ReactDOM.render(
     <Provider store={store}>
-        <AppErrorBoundary>
-            <App />
-        </AppErrorBoundary>
+        <BrowserRouter>
+            <AppErrorBoundary>
+                <App />
+            </AppErrorBoundary>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
