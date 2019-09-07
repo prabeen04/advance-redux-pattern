6e173534a487ebe08d42a52468df4e138143117d
Prabeen04 <prabeen.strange@gmail.com>
2019-09-08 00:20:10 +0530

    TinyLine chart added in JumpStart box

--- src/components/UI/Elements/JumpStartBox.js
+++ src/components/UI/Elements/JumpStartBox.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 import styled from 'styled-components';
+import { LineChart, Line, ResponsiveContainer, Tooltip } from 'recharts';
+const data = [
+    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+];
 export default function JumpStartBox(props) {
     return (
         <JumpStartWrapper>
@@ -15,6 +25,12 @@ export default function JumpStartBox(props) {
                     }
                     0.37
                 </Progress>
+                <ChartContainer>
+                    <LineChart width={100} data={data}>
+                        <Line type='monotone' dataKey='pv' stroke='#8884d8' strokeWidth={1} />
+                        <Tooltip/>
+                    </LineChart>
+                </ChartContainer>
             </Container>
         </JumpStartWrapper>
     )
@@ -55,4 +71,8 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+`;
+
+const ChartContainer = styled(ResponsiveContainer)`
+    margin-left: 1.5rem;
 `;
