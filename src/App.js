402f3e362615a8066a76aef91e100fa9979e6efb
Prabeen04 <prabeen.strange@gmail.com>
2019-09-07 22:04:52 +0530

    muitheme removed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { ThemeProvider } from "styled-components";
 import { ApplicationWrapper } from "./components/UI/Layout";
 import './App.css';
 import ThemeSwitch from './modules/settings/theme/ThemeSwitch';
+import { Button } from '@material-ui/core';
 
 function App() {
   const theme = useSelector(({ theme }) => theme.theme)
   return (
     <ThemeProvider theme={theme}>
       <ApplicationWrapper>
+        <Button color='primary' variant='contained'>Click me</Button>
         <h1>Advance redux pattern</h1>
         <ThemeSwitch />
       </ApplicationWrapper>
