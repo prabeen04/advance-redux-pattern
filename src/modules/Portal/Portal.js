a3e839b53cf7e0e509be26488579ebadda2f94e4
Prabeen04 <prabeen.strange@gmail.com>
2019-09-20 20:16:47 +0530

    Portal component created

---
+++ src/modules/Portal/Portal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Portal() {
+  return (
+    <div>
+      <h1>Portal component</h1>
+    </div>
+  )
+}
--- src/modules/Routes/Routes.js
+++ src/modules/Routes/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom";
+import Blog from '../Blog/Blog';
+import Portal from '../Portal/Portal';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path='/' component={null} />
-      <Route exact path='/blog' component={null} />
-      <Route exact path='/portal' component={null} />
+      <Route exact path='/' component={Blog} />
+      <Route exact path='/blog' component={<Blog />} />
+      <Route exact path='/portal' component={<Portal />} />
     </Switch>
   )
 }
