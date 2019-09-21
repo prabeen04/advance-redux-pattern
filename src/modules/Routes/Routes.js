441ed598d21e302694e3f1d90c0c010ae15f0d3a
Prabeen04 <prabeen.strange@gmail.com>
2019-09-21 21:42:44 +0530

    Home component

--- src/modules/Routes/Routes.js
+++ src/modules/Routes/Routes.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Switch, Route } from "react-router-dom";
 import Blog from '../Blog/Blog';
 import Portal from '../Portal/Portal';
+import Home from '../Home/Home';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path='/' component={Blog} />
-      <Route exact path='/blog' component={<Blog />} />
-      <Route exact path='/portal' component={<Portal />} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/blog' component={Blog} />
+      <Route exact path='/portal' component={Portal} />
     </Switch>
   )
 }
