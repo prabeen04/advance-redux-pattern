61d03ba038d201ddc1ddf57c315951491cdac818
Prabeen04 <prabeen.strange@gmail.com>
2019-10-01 23:11:01 +0530

    useFonoApi deleted

--- src/Hooks/useFonoApi.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react'
-const url = 'https://fonoapi.freshpixl.com/v1/getAdvice'
-const token = '1a0fc4ad0d1c4f0ba86a99cbfc9db19535b9145bb5bd865b'
-export default function useFonoApi() {
-  const [data, setData] = useState([])
-  return data
-}
