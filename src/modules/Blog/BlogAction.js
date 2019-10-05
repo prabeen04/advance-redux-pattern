89cb366b22d00fc751b4956216ad7aaaef38a6c5
Prabeen04 <prabeen.strange@gmail.com>
2019-10-05 20:34:18 +0530

    blogById selector, action and service created

--- src/modules/Blog/BlogAction.js
+++ src/modules/Blog/BlogAction.js
@@ -1,7 +1,10 @@
 import { createAction } from "../../store/utils/createAction"
-import { GET_BLOGS } from "./BlogActionTypes"
+import { GET_BLOGS, GET_BLOG_BY_ID } from "./BlogActionTypes"
 import BlogService from "./BlogService"
 
 export const getBlogs = () => {
   return createAction(GET_BLOGS, () => BlogService.getBlog())
+}
+export const getBlogById = id => {
+  return createAction(GET_BLOG_BY_ID, () => BlogService.getBlogById(id))
 }
--- src/modules/Blog/BlogSelector.js
+++ src/modules/Blog/BlogSelector.js
@@ -1,3 +1,4 @@
 import { STATES } from "../../store/rootReducer";
 
 export const selectAllBlogs = state => state[STATES.BLOG].list
+export const selectBlogById = state => state(STATES.BLOG).read
