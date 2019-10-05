3687efa9acc53bdc9e901a002aa203e945f3f24e
Prabeen04 <prabeen.strange@gmail.com>
2019-10-05 20:52:56 +0530

    blog id shown

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
-export default function BlogDetail() {
+import { useParams } from "react-router-dom";
+export default function BlogDetail(props) {
+  const params = useParams()
   return (
     <div>
       <h2>Blog Detail component</h2>
+      <p>blog id: {params.id}</p>
     </div>
   )
 }
