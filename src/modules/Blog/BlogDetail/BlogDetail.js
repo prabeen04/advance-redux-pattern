29c9939d62f21f7de996208afccf6e973dfe964d
Prabeen04 <prabeen.strange@gmail.com>
2019-10-06 18:12:14 +0530

    Blog page updated after clicking next

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import { selectBlogById } from '../BlogSelector';
@@ -7,16 +7,22 @@ export default function BlogDetail(props) {
   const { id } = useParams()
   const history = useHistory()
   const dispatch = useDispatch()
+  const [blogId, setBlogId] = useState(id)
   const { data, isLoading } = useSelector(selectBlogById)
   useEffect(() => {
     dispatch(getBlogById(id))
-  }, [id])
+  }, [blogId])
   if (!data) return null
   if (isLoading) return <p>Loading...</p>
   return (
     <div>
       <h2>Blog Detail component</h2>
-      <button onClick={() => history.push(`/${id++}`)}>next</button>
+      <button onClick={() => {
+        setBlogId(Number(blogId) + 1)
+        history.push(`/blog/${blogId}`)
+      }
+      }
+      >next</button>
       <button>prev</button>
       <p>ID: {data.id}</p>
       <p>TITLE: {data.title}</p>
