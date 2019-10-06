2902cab25bac414905fe7accf3d6921a8af8f7f6
Prabeen04 <prabeen.strange@gmail.com>
2019-10-06 17:10:42 +0530

    next/prev button addded

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { selectBlogById } from '../BlogSelector';
 import { getBlogById } from '../BlogAction';
 export default function BlogDetail(props) {
   const { id } = useParams()
+  const history = useHistory()
   const dispatch = useDispatch()
   const { data, isLoading } = useSelector(selectBlogById)
   useEffect(() => {
@@ -15,6 +16,8 @@ export default function BlogDetail(props) {
   return (
     <div>
       <h2>Blog Detail component</h2>
+      <button onClick={() => history.push(`/${id++}`)}>next</button>
+      <button>prev</button>
       <p>ID: {data.id}</p>
       <p>TITLE: {data.title}</p>
       <p>DESCRIPTION: {data.description}</p>
