901a609d0b3ef0fc886520a5074aa7541cc879f8
Prabeen04 <prabeen.strange@gmail.com>
2019-10-06 18:13:03 +0530

    changes reverted

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { selectBlogById } from '../BlogSelector';
 import { getBlogById } from '../BlogAction';
 export default function BlogDetail(props) {
   const { id } = useParams()
-  const history = useHistory()
   const dispatch = useDispatch()
-  const [blogId, setBlogId] = useState(id)
   const { data, isLoading } = useSelector(selectBlogById)
   useEffect(() => {
     dispatch(getBlogById(id))
-  }, [blogId])
+  }, [id])
   if (!data) return null
   if (isLoading) return <p>Loading...</p>
   return (
     <div>
       <h2>Blog Detail component</h2>
-      <button onClick={() => {
-        setBlogId(Number(blogId) + 1)
-        history.push(`/blog/${blogId}`)
-      }
-      }
-      >next</button>
-      <button>prev</button>
       <p>ID: {data.id}</p>
       <p>TITLE: {data.title}</p>
       <p>DESCRIPTION: {data.description}</p>
