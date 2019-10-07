4581d7f779efb256b09006732eb3adc115f7f70a
Prabeen04 <prabeen.strange@gmail.com>
2019-10-07 22:21:14 +0530

    Prev button added

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Button } from "@material-ui/core";
-import { selectBlogById } from '../BlogSelector';
-import { getBlogById } from '../BlogAction';
+import { selectBlogById, selectAllBlogs } from '../BlogSelector';
+import { getBlogById, getBlogs, } from '../BlogAction';
 export default function BlogDetail(props) {
   const { id } = useParams()
   const dispatch = useDispatch()
-  const [blogId, setBlogId] = useState(id)
+  const [blogId, setBlogId] = useState(null)
   const { data, isLoading } = useSelector(selectBlogById)
+  const blogList = useSelector(selectAllBlogs).data
+  debugger
+
+  useEffect(() => {
+    setBlogId(id)
+    dispatch(getBlogs())
+  }, [])
   useEffect(() => {
     dispatch(getBlogById(blogId))
   }, [blogId])
@@ -16,10 +23,22 @@ export default function BlogDetail(props) {
   if (isLoading) return <p>Loading...</p>
   return (
     <div>
-      <Button variant="contained" onClick={() => {
-        setBlogId(Number(blogId) + 1)
-        props.history.push(`/blog/${Number(blogId) + 1}`)
-      }}>Next</Button>
+      <Button
+        color='primary'
+        disabled={blogId > blogList.length}
+        variant="contained"
+        onClick={() => {
+          setBlogId(Number(blogId) - 1)
+          props.history.push(`/blog/${Number(blogId) - 1}`)
+        }}>prev</Button>
+      <Button
+        color='primary'
+        disabled={blogId == blogList.length || blogId < 0}
+        variant="contained"
+        onClick={() => {
+          setBlogId(Number(blogId) + 1)
+          props.history.push(`/blog/${Number(blogId) + 1}`)
+        }}>Next</Button>
       <h2>Blog Detail component</h2>
       <p>ID: {data.id}</p>
       <p>TITLE: {data.title}</p>
