41c551b9dddf47992f0975585aedaa0f4b0e9565
Prabeen04 <prabeen.strange@gmail.com>
2019-10-18 07:39:47 +0530

    logggerMiddleware created

---
+++ src/middleware/loggerMiddleware.js
@@ -0,0 +1,5 @@
+const loggerMiddleware = store => next => action => {
+  console.log(action);
+  return next(action);
+};
+export default loggerMiddleware;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-import GAMiddleware from '../middleware/googleAnalytics';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+import GAMiddleware from "../middleware/googleAnalytics";
+import loggerMiddleware from "../middleware/loggerMiddleware";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk, GAMiddleware)));
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk, GAMiddleware, loggerMiddleware))
+);
 export default store;
