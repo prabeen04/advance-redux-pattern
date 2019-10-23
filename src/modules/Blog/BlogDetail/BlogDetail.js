3897f8317c71bb7899a06e01f9a0857ab44dfbf7
Prabeen04 <prabeen.strange@gmail.com>
2019-10-23 20:06:24 +0530

    Breadcrumbs added in BlogDetail

--- src/modules/Blog/BlogDetail/BlogDetail.js
+++ src/modules/Blog/BlogDetail/BlogDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, Breadcrumbs, Link, Typography } from "@material-ui/core";
 import styled from "styled-components";
 import { selectBlogById, selectAllBlogs } from "../BlogSelector";
 import { getBlogById, getBlogs } from "../BlogAction";
@@ -31,7 +31,16 @@ export default function BlogDetail(props) {
   return (
     <div>
       <HeaderContainer>
-        <KeyboardReturnIcon onClick={() => props.history.push(`/blog`)} />
+        <div style={{ display: "flex" }}>
+          <KeyboardReturnIcon onClick={() => props.history.push(`/blog`)} />{" "}
+          &nbsp;
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link color="inherit" onClick={() => props.history.push("/blog")}>
+              Blog
+            </Link>
+            <Typography color="textPrimary">{blogId}</Typography>
+          </Breadcrumbs>
+        </div>
         <div>
           <ArrowBackIosIcon
             disabled={blogId > blogList.length || blogId == 1}
