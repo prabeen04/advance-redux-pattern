9f854fce90bd420c78b6f75fd02edb3edfa640c0
Prabeen04 <prabeen.strange@gmail.com>
2019-10-25 22:39:30 +0530

    MenuItem styles higlighted

--- src/modules/Blog/BlogDetail/BlogMenu.js
+++ src/modules/Blog/BlogDetail/BlogMenu.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Card from "../../../components/UI/Layout/Card";
 import { getBlogs } from "../BlogAction";
@@ -9,6 +9,7 @@ import { selectAllBlogs } from "../BlogSelector";
 export default function BlogMenu() {
   const { data, isLoading } = useSelector(selectAllBlogs);
   const dispatch = useDispatch();
+  const params = useParams();
   debugger;
   useEffect(() => {
     dispatch(getBlogs());
@@ -17,7 +18,11 @@ export default function BlogMenu() {
     <Card>
       <LinkContainer>
         {data &&
-          data.map(d => <LinkItem to={`/blog/${d.id}`}>{d.title}</LinkItem>)}
+          data.map(d => (
+            <LinkItem to={`/blog/${d.id}`} id={d.id} pId={params.id}>
+              {d.title}
+            </LinkItem>
+          ))}
       </LinkContainer>
     </Card>
   );
@@ -27,9 +32,18 @@ const LinkContainer = styled.div`
   flex-direction: column;
 `;
 const LinkItem = styled(Link)`
-  padding: 0.3rem 0.7rem;
+  padding: 0.5rem 0.3rem;
   text-decoration: none;
-  background-color: #f4f4f4;
-  border-radius: 0.3rem;
+  background-color: ${props => (props.id == props.pId ? "#ddd" : "#f4f4f4")};
+  border-radius: 0.2rem;
   margin: 0.1rem;
+  font-size: 1rem;
+  color: #666;
+  letter-spacing: 1;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover {
+    background-color: #ddd;
+    color: #444;
+  }
 `;
