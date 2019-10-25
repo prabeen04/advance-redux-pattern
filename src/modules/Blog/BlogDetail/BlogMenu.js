c1bd9d2422b6233e88ba87b5779953189739f225
Prabeen04 <prabeen.strange@gmail.com>
2019-10-25 21:47:11 +0530

    link styles

--- src/modules/Blog/BlogDetail/BlogMenu.js
+++ src/modules/Blog/BlogDetail/BlogMenu.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Card from "../../../components/UI/Layout/Card";
 import { getBlogs } from "../BlogAction";
 import { selectAllBlogs } from "../BlogSelector";
@@ -11,5 +13,23 @@ export default function BlogMenu() {
   useEffect(() => {
     dispatch(getBlogs());
   }, []);
-  return <Card>{data && data.map(d => <p>{d.title}</p>)}</Card>;
+  return (
+    <Card>
+      <LinkContainer>
+        {data &&
+          data.map(d => <LinkItem to={`/blog/${d.id}`}>{d.title}</LinkItem>)}
+      </LinkContainer>
+    </Card>
+  );
 }
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const LinkItem = styled(Link)`
+  padding: 0.3rem 0.7rem;
+  text-decoration: none;
+  background-color: #f4f4f4;
+  border-radius: 0.3rem;
+  margin: 0.1rem;
+`;
