4f8de887bc466ed2516196d65577f27fde6f2b4d
Prabeen04 <prabeen.strange@gmail.com>
2019-10-28 21:57:04 +0530

    jumpstartbox responsive

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -5,20 +5,33 @@ import { JumpStartBox } from "../../components/UI/Elements";
 
 export default function Home() {
   return (
-    <CurveyBackgroundHeader>
+    <div style={{ position: "relative" }}>
+      <CurveyBackgroundHeader />
       <JSWrapper>
         <JumpStartBox value={300} title="Goals scored" />
         <JumpStartBox value={28} title="Total wins" />
         <JumpStartBox value={5} title="Total lost" />
       </JSWrapper>
-    </CurveyBackgroundHeader>
+    </div>
   );
 }
 
 const JSWrapper = styled.div`
+  width: 100%;
   height: 100%;
   z-index: 999;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  top: 0;
+  /* mobile device */
+  @media (max-width: 600px) {
+    flex-direction: column;
+    top: 50px;
+    & > * {
+      width: 100%;
+      margin: 0.5rem;
+    }
+  }
 `;
