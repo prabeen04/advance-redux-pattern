2beffd6bccff9429a3c7a1cd5bc0cd507280bbbc
Prabeen04 <prabeen.strange@gmail.com>
2019-10-29 20:04:50 +0530

    small stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { ApplicationWrapper } from "./components/UI/Layout";
-import Navbar from './modules/Nav/Navbar';
-import Routes from './modules/Routes/Routes';
-import './App.css';
+import Navbar from "./modules/Nav/Navbar";
+import Routes from "./modules/Routes/Routes";
+import "./App.css";
 
 function App() {
-  const theme = useSelector(({ theme }) => theme.theme)
+  const theme = useSelector(({ theme }) => theme.theme);
   return (
     <ThemeProvider theme={theme}>
       <ApplicationWrapper>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           <Navbar />
         </div>
         <Routes />
