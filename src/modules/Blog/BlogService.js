7b057ca513e8340cff7e628e18bde0a6ea0b38aa
Prabeen04 <prabeen.strange@gmail.com>
2019-11-03 22:18:51 +0530

    dummy content added for blogs

--- src/modules/Blog/BlogService.js
+++ src/modules/Blog/BlogService.js
@@ -4,21 +4,67 @@ const blogs = [
   {
     id: 1,
     title: "Title1",
-    description: "Description 1 lorem pixel losus erprie sekjrn"
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
   },
-  { id: 2, title: "Title2", description: "Description 2" },
-  { id: 3, title: "Title3", description: "Description 3" },
-  { id: 4, title: "Title4", description: "Description 4" },
-  { id: 5, title: "Title5", description: "Description 5" }
+  {
+    id: 2,
+    title: "Title2",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
+  },
+  {
+    id: 3,
+    title: "Title3",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
+  },
+  {
+    id: 4,
+    title: "Title4",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
+  },
+  {
+    id: 5,
+    title: "Title5",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
+  }
 ];
 const timeout = ms => new Promise(r => setTimeout(r, ms));
 const BlogService = {
   async getBlog() {
-    await timeout(2000);
+    await timeout(500);
     return BlogModel.fromAll(blogs);
   },
   async getBlogById(id) {
-    await timeout(2000);
+    await timeout(500);
     const currentBlog = blogs.find(blog => blog.id == id);
     return BlogModel.from(currentBlog);
   }
