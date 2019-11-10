97faee13fc0e9f74502ab545759c35b07aa78860
Prabeen04 <prabeen.strange@gmail.com>
2019-11-10 20:01:11 +0530

    Create blog fired

--- src/modules/Blog/BlogModal/BlogModal.js
+++ src/modules/Blog/BlogModal/BlogModal.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import StyledModal from "../../../components/UI/MUI/StyledModal";
+import { Button } from "@material-ui/core";
+import { createBlog } from "../BlogAction";
+
+const data = {
+  id: 6,
+  title: "Title1",
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+     unknown printer took a galley of type and scrambled it to make a type specimen book. 
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of 
+      Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+       software like Aldus PageMaker including versions of Lorem Ipsum.`
+};
 
 export default function BlogModal(props) {
   const { open, toggle } = props;
+  const dispatch = useDispatch();
   return (
     <>
       <StyledModal open={open} handleClose={() => toggle(false)}>
-        <h1>Modal content</h1>
+        <Button onClick={() => dispatch(createBlog(data))}>ADD BLOG</Button>
       </StyledModal>
     </>
   );
