3957fe4e87633d7bf7713395713ee901b1a7014a
Prabeen04 <prabeen.strange@gmail.com>
2019-11-12 20:47:30 +0530

    SuspenseList added

--- src/modules/Concurrent/Concurrent.js
+++ src/modules/Concurrent/Concurrent.js
@@ -1,14 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, SuspenseList } from "react";
 import ProfileDetails from "./CCUser";
 import ProfileTimeline from "./CCPost";
 
 export default function Concurrent() {
   return (
-    <Suspense fallback={<h1>Loading profile...</h1>}>
-      <ProfileDetails />
-      <Suspense fallback={<h3>Loading posts...</h3>}>
-        <ProfileTimeline />
-      </Suspense>
-    </Suspense>
+    <>
+      <SuspenseList>
+        <Suspense fallback={<h1>Loading profile...</h1>}>
+          <ProfileDetails />
+        </Suspense>
+        <Suspense fallback={<h3>Loading posts...</h3>}>
+          <ProfileTimeline />
+        </Suspense>
+      </SuspenseList>
+    </>
   );
 }
